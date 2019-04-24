3d3968cbd02b146130d687c42ba14c7ee5f5daac
Josh Holland <anowlcalledjosh@gmail.com>
2019-04-24 15:13:33 +0100

    Add -q option

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 #[macro_use]
 extern crate clap;
+#[macro_use]
+extern crate failure;
 
 use std::fs;
-use std::process::Command;
-use clap::{App, AppSettings, SubCommand};
+use std::process::{Command, exit};
+use clap::{App, AppSettings, Arg, SubCommand};
+use failure::Error;
+
+#[derive(Debug, Fail)]
+enum BbswitchError {
+    #[fail(display = "bbswitch not available")]
+    BbswitchNotAvailable {
+        #[fail(cause)]
+        cause: std::io::Error,
+    },
+    #[fail(display = "unknown bbswitch state")]
+    UnknownBbswitchState {
+        state: String,
+    },
+}
 
 fn main() {
     let matches = App::new("nvctl")
@@ -16,6 +32,13 @@ fn main() {
         .setting(AppSettings::DisableHelpSubcommand)
         .help_message("Display this help message")
         .version_message("Display version information")
+        .arg(
+            Arg::with_name("quiet")
+                .short("q")
+                .long("quiet")
+                .help("Don't yell on error")
+                .global(true)
+        )
         .subcommand(
             SubCommand::with_name("power")
             .about("Query and configure the discrete GPU's power state")
@@ -48,7 +71,7 @@ fn main() {
         )
         .get_matches();
 
-    match matches.subcommand() {
+    match match matches.subcommand() {
         ("power", Some(matches)) => match matches.subcommand() {
             ("on", _) => power_on(),
             ("off", _) => power_off(),
@@ -62,31 +85,46 @@ fn main() {
             _ => unreachable!(),
         },
         _ => unreachable!(),
+    } {
+        Ok(_) => {},
+        Err(e) => {
+            if !matches.is_present("quiet") {
+                println!("error: {}", e.cause())
+            }
+            exit(1)
+        },
     }
 }
 
-fn bbswitch_write(data: &str) {
-    fs::write("/proc/acpi/bbswitch", data).expect("couldn't trigger bbswitch");
+fn bbswitch_read() -> Result<String, BbswitchError> {
+    fs::read_to_string("/proc/acpi/bbswitch").map_err(|e| BbswitchError::BbswitchNotAvailable { cause: e })
 }
 
-fn power_on() {
-    bbswitch_write("ON");
+fn bbswitch_write(data: &str) -> Result<(), BbswitchError> {
+    fs::write("/proc/acpi/bbswitch", data).map_err(|e| BbswitchError::BbswitchNotAvailable { cause: e })
 }
 
-fn power_off() {
-    bbswitch_write("OFF");
+fn power_on() -> Result<(), Error> {
+    Ok(bbswitch_write("ON")?)
 }
 
-fn power_query() {
-    let contents = fs::read_to_string("/proc/acpi/bbswitch").expect("bbswitch not available");
+fn power_off() -> Result<(), Error> {
+    Ok(bbswitch_write("OFF")?)
+}
+
+fn power_query() -> Result<(), Error> {
+    let contents = bbswitch_read()?;
     let contents = contents.trim();
     if contents.ends_with("ON") {
         println!("on");
     } else if contents.ends_with("OFF") {
         println!("off");
     } else {
-        panic!("unknown bbswitch state: {}", contents);
+        return Err(BbswitchError::UnknownBbswitchState {
+            state: contents.to_owned(),
+        }.into());
     }
+    Ok(())
 }
 
 fn prime_select(mode: &str) -> Vec<u8> {
@@ -100,18 +138,21 @@ fn prime_select(mode: &str) -> Vec<u8> {
     output.stdout
 }
 
-fn gpu_intel() {
+fn gpu_intel() -> Result<(), Error> {
     prime_select("intel");
+    Ok(())
 }
 
-fn gpu_nvidia() {
+fn gpu_nvidia() -> Result<(), Error> {
     prime_select("nvidia");
+    Ok(())
 }
 
-fn gpu_query() {
+fn gpu_query() -> Result<(), Error> {
     println!("{}", match String::from_utf8_lossy(&prime_select("query")).trim() {
         "intel" => "intel",
         "nvidia" => "nvidia",
         s => panic!("unknown GPU name: {}", s),
-    })
+    });
+    Ok(())
 }
